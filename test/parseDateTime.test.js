c5415765363208a541f54e980ffe0e4201a0ce26
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-06-14 12:35:35 +0200

    test: remove useless test cases

--- test/parseDateTime.test.js
+++ test/parseDateTime.test.js
@@ -6,18 +6,6 @@ describe('parseDateTime', () => {
     assert.strictEqual(typeof parseDateTime, 'function')
   })
 
-  it('should parse a date time string', () => {
-    const dateTime = parseDateTime('2018-01-01T01:00:00.000+0100')
-
-    assert.strictEqual(dateTime.toUTC().toISO(), '2018-01-01T00:00:00.000Z')
-  })
-
-  it('should parse a date time string and set the given timezone', () => {
-    const dateTime = parseDateTime('2018-01-01T01:00:00', null, 'Europe/Berlin')
-
-    assert.strictEqual(dateTime.toUTC().toISO(), '2018-01-01T00:00:00.000Z')
-  })
-
   it('should parse a date time string using the format argument', () => {
     const dateTime = parseDateTime('20180101 000000', 'yyyyMMdd HHmmss', 'UTC')
 
